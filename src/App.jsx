dc894d183e010edba9f5e54de73652c0859e447d
Ashley Yang <ashley.minghui@gmail.com>
2025-06-06 09:33:48 -0700

    Add buttons with event handlers that update state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    const [color, setColor] = useState("red");
+  
+    return (
+      <div className="App">
+        <p>Which color do you prefer?</p>
+        <button id="red-button" onClick={(e) => setColor("red")}>Red</button>
+        <button id="green-button" onClick={(e) => setColor("green")}>Green</button>
+        <p className={color}>You prefer {color}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+  }
 
 export default App
